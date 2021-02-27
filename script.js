9f618461c07d6a405f90275d9493a6afafa34ec7
Altin Thaqi <ltin.thaqi.520@gmail.com>
2021-02-27 21:37:23 +0100

    Implemented Score feature, winning and lossing the game

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ console.log((document.querySelector('.guess').value = 10));
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector('.number').textContent = secretNumber;
+let score = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -23,8 +24,22 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess == secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!';
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = 'Too High!';
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too High!';
+      score--;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = '0';
+    }
+    document.querySelector('.score').textContent = score;
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = 'Too Low!';
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too Low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = '0';
+    }
   }
 });
