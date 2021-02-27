8896ea037e164123b221961314d5581a2ba0815e
Altin Thaqi <ltin.thaqi.520@gmail.com>
2021-02-27 21:23:55 +0100

    Implemented check event listener and implemented the message scenarious with DOM

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 'use strict';
 
+/*
+
 console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+console.log((document.querySelector('.guess').value = 10));
+
+*/
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess, guess);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+  } else if (guess == secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too High!';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too Low!';
+  }
+});
