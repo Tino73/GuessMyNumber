9014dfd6d91f28c87196c7ab9e5b49b2387ba95f
Altin Thaqi <ltin.thaqi.520@gmail.com>
2021-02-27 22:09:50 +0100

    Implemented Highscore feature

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ console.log((document.querySelector('.guess').value = 10));
 */
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 2;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -29,6 +30,10 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'Correct Number!';
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
   }
 
   //When guess it too high
@@ -56,6 +61,7 @@ document.querySelector('.check').addEventListener('click', function () {
   }
 });
 
+//Start a new game
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
